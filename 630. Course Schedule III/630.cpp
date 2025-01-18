43b26ce101d9888714368cc2145c1a11fbff23c7
Cássio Reis <csreis04@gmail.com>
2025-01-18 17:10:13 -0300

    Implementa seleção de cursos, removendo o mais longo ao exceder prazo

--- 630. Course Schedule III/630.cpp
+++ 630. Course Schedule III/630.cpp
@@ -1,19 +1,40 @@
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 class Solution {
 public:
     // Compara os cursos pelo último dia
-    bool compareByLastDay(vector<int> course1, vector<int> course2) {
+    static bool compareByLastDay(vector<int>& course1, vector<int>& course2) {
         return course1[1] < course2[1];
     }
-
+    
     int scheduleCourse(vector<vector<int>>& courses) {
         // Ordenar os cursos pelo último dia
         // Ideia similiar ao interval scheduling
         sort(courses.begin(), courses.end(), compareByLastDay);
 
         
+        int time = 0; // Tempo total acumulado dos cursos selecionados
+        priority_queue<int> maxHeap; // Max-heap para armazenar as durações dos cursos selecionados
+
+        // Seleção dos cursos
+        for (auto& course : courses) {
+            int duration = course[0];
+            int deadline = course[1];
+
+            maxHeap.push(duration);
+            time += duration;
+
+            // Se o tempo total ultrapassar o prazo, removemos o curso mais longo
+            // para liberar tempo e maximizar o número de cursos selecionados.
+            if (time > deadline) {
+                time -= maxHeap.top();
+                maxHeap.pop();
+            }
+        }
+        
+        return maxHeap.size();
     }
 };
