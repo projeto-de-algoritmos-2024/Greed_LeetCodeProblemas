182f3a39e6f1b8cc31130d461932600140c51fb2
Cássio Reis <csreis04@gmail.com>
2025-01-18 16:35:08 -0300

    Ordena os cursos pelo último dia

--- 630. Course Schedule III/630.cpp
+++ 630. Course Schedule III/630.cpp
@@ -1,9 +1,19 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
+    // Compara os cursos pelo último dia
+    bool compareByLastDay(vector<int> course1, vector<int> course2) {
+        return course1[1] < course2[1];
+    }
+
     int scheduleCourse(vector<vector<int>>& courses) {
+        // Ordenar os cursos pelo último dia
+        // Ideia similiar ao interval scheduling
+        sort(courses.begin(), courses.end(), compareByLastDay);
+
         
     }
 };
